a28e36daac067147d6a6b6e5386466aa4a4d6de1
PinkemenaP <138155829+PinkemenaP@users.noreply.github.com>
2023-06-29 15:30:06 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,15 +9,11 @@ function draw() {
   background(245,87,170); //an RGB color for the canvas' background
   // //circle
   //make  the ellipse follow your mouse 
-  stroke(245,80,90,255); // an RGB color for the circle's border
-  strokeWeight(7);
-  fill(240,152,180,255); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  stroke(245,80,90); // an RGB color for the circle's border
+  strokeWeight(5);
+  fill(270,152,180); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
   ellipse(mouseX,mouseY,20,20);
-  ellipse(mouseX+30,mouseY,20,20);
+  ellipse(mouseX+40,mouseY,20,20);
   // Draw the shape
   
- 
-
 }
-
-
